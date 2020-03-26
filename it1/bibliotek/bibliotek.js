f7970ddb6bda5e27bccfa6dfeece475c2fd98215
sava <sava@420blaze.it>
2020-03-26 02:30:12 +0100

    basj i buksa, biblioteket e oppegående

--- it1/bibliotek/bibliotek.js
+++ it1/bibliotek/bibliotek.js
@@ -1,4 +1,15 @@
 
-db = init().firestore()
-bibliotekcoll = db.collection("bibliotek")
+var add = function() {
+    db.collection("bibliotek").add({
+        tittel: $('#title')[0].value,
+        forfatter: $('#author')[0].value,
+        forlag: $('#publisher')[0].value,
+        terningkast: $('#rating')[0].value,
+        utgitt: $('#published')[0].value
+    }).then(function() {
+        $('#status').html('<p style="color: green;">La til bok '+$('#title').value+'!</p>')
+    }).catch(function() {
+        $('#status').html('<p style="color: red;">Problem med å legge til bok '+$('#title').value+'!</p>')
+    })
+}
 
---
+++ it1/navbar/navbar.js
@@ -0,0 +1,23 @@
+$('#navbar').parent().html(`
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" target="_top" href="/it1/index.html">IT1 firebase stuff</a> <!-- href="/it1/index.html" -->
+    <!-- <ul class="navbar-nav">
+        <li class="nav-item right">
+            <a class="nav-link" target="_top" href="/it1/about.html">About</a>
+        </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item right">
+            <a class="nav-link">Simple login here later</a>
+        </li>
+        <li class="nav-item right" style="visibility: hidden">
+            <a class="nav-link" target="_top" href="/logout">Logout</a>
+        </li>
+        <li class="nav-item right" id="register-btn">
+            <a class="nav-link" target="_top" href="/register">Register</a>
+        </li>
+        <li class="nav-item right" id="login-btn">
+            <a class="nav-link" target="_top" href="/login">Login</a>
+        </li>
+    </ul> -->
+</nav>`)
