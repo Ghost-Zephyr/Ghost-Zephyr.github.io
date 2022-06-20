9dd93e26bebae7d0074887a77b275a97cf99f64b
Ghost-Zephyr <Ghost-Zephyr@users.noreply.github.com>
2022-06-20 14:21:52 +0000

    deploy: 10dca2972d5777e0fd17ec333210a342241ade84

--- apprentice/handlebars.min.ccdbfd969896a47327e9a3cdf797b56b82fc5271aa23d26d1344a9f505672b5e.js
+++ apprentice/handlebars.min.ccdbfd969896a47327e9a3cdf797b56b82fc5271aa23d26d1344a9f505672b5e.js
@@ -7,7 +7,7 @@ inspect element here!`)(),nested=Handlebars.compile(`
   {{#each links}}
   <li><a href='{{href}}' target='_blank'>{{text}}</a></li>
   {{/each}}
-</ul>`)({links:[{href:'https://handlebarsjs.com/guide',text:'Handlebars official guide'},{href:'https://io.sivert.pw',text:'Awesome blog!'}]});Handlebars.registerHelper('censor',e=>e.replaceAll('fuck','f***').replaceAll('hell','h***')),Handlebars.registerHelper('noop',e=>e.fn(this)),Handlebars.registerPartial('people',`
+</ul>`)({links:[{href:"https://handlebarsjs.com/guide",text:"Handlebars official guide"},{href:"https://io.sivert.pw",text:"Awesome blog!"}]});Handlebars.registerHelper("censor",e=>e.replaceAll("fuck","f***").replaceAll("hell","h***")),Handlebars.registerHelper("noop",e=>e.fn(this)),Handlebars.registerPartial("people",`
 <ul>
   {{#each members}}
   <li>{{name}} age {{age}} from {{home}}</li>
@@ -23,7 +23,7 @@ inspect element here!`)(),nested=Handlebars.compile(`
   {{#each members}}
   <li>{{name}} age {{age}} from {{home}}</li>
   {{/each}}
-</ul>`)({profanity:'fuckin hell man!',members:[{home:'Never Never Land',name:'Peter',age:120},{home:'Trollhättan',name:'Clark',age:75}]});document.addEventListener("DOMContentLoaded",function(){let e=document.createElement('div');e.id='output',document.getElementById('meta').append(e),Handlebars.registerPartial('layout','<br /><div>{{> content }}</div>');let t=Handlebars.compile(`
+</ul>`)({profanity:"fuckin hell man!",members:[{home:"Never Never Land",name:"Peter",age:120},{home:"Trollhättan",name:"Clark",age:75}]});document.addEventListener("DOMContentLoaded",function(){let e=document.createElement("div");e.id="output",document.getElementById("meta").append(e),Handlebars.registerPartial("layout","<br /><div>{{> content }}</div>");let t=Handlebars.compile(`
 {{#> layout}}
   {{#*inline "content"}}
     <h4>Examples output:</h4>
